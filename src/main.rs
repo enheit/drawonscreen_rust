0a4f2541faf1f14d7b9cdc8c2063cbb120dc0509
Roman Mahotskyi <roman.mahotskyi@gmail.com>
2025-06-05 12:30:27 +0300

    void: save

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,10 @@ use softbuffer::{Context, Rect, Surface};
 use winit::{
     application::ApplicationHandler,
     event::{self, ElementState, MouseButton, WindowEvent},
-    event_loop::EventLoop,
+    event_loop::{ActiveEventLoop, EventLoop},
     keyboard::{KeyCode, PhysicalKey},
-    window::{Window, WindowAttributes},
+    window::{Window, WindowAttributes, WindowId},
 };
-
-const WIDTH: usize = 100;
-const HEIGHT: usize = 100;
 
 #[derive(PartialEq, Eq)]
 enum DrawState {
@@ -25,6 +22,7 @@ enum ColorState {
     Green = 0x0000ff00,
     Blue = 0x000000ff,
     White = 0xffffffff,
+    Black = 0xff000000,
 }
 
 struct DrawOnScreen {
@@ -55,8 +53,10 @@ impl Default for DrawOnScreen {
 }
 
 impl ApplicationHandler for DrawOnScreen {
-    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
-        let window_attributes = WindowAttributes::default().with_title("Draw On Screen");
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window_attributes = WindowAttributes::default()
+            .with_transparent(true)
+            .with_title("Draw On Screen");
 
         let window = event_loop.create_window(window_attributes);
 
@@ -101,8 +101,8 @@ impl ApplicationHandler for DrawOnScreen {
 
     fn window_event(
         &mut self,
-        event_loop: &winit::event_loop::ActiveEventLoop,
-        window_id: winit::window::WindowId,
+        event_loop: &ActiveEventLoop,
+        window_id: WindowId,
         event: event::WindowEvent,
     ) {
         match event {
@@ -152,6 +152,11 @@ impl ApplicationHandler for DrawOnScreen {
                         self.color_state = ColorState::Blue;
                         println!("Blue");
                     }
+
+                    if event.physical_key == PhysicalKey::Code(KeyCode::Digit5) {
+                        self.color_state = ColorState::Black;
+                        println!("Black");
+                    }
                 }
             }
             WindowEvent::CursorMoved {
@@ -160,113 +165,48 @@ impl ApplicationHandler for DrawOnScreen {
             } => {
                 if let Some(window) = &self.window {
                     if self.draw_state == DrawState::Drawing {
-                        let window_size = window.inner_size();
-                        let width = window_size.width as usize;
-                        let height = window_size.height as usize;
-                        let x = position.x as usize;
-                        let y = position.y as usize;
+                        let index = (position.y as usize * window.inner_size().width as usize)
+                            + position.x as usize;
 
-                        if let Some(prev_pos) = self.last_position {
-                            let px = prev_pos.x as usize;
-                            let py = prev_pos.y as usize;
+                        self.canvas[index] = self.color_state as u32;
 
-                            // Call this before any mutable borrows
-                            let line = self.draw_line((px, py), (x, y), width, height);
+                        println!(
+                            "[CursorMoved]: Position is {}x{} (i - {})",
+                            position.x, position.y, index
+                        );
 
-                            // Draw to canvas
-                            for (lx, ly) in &line {
-                                let index = ly * width + lx;
-                                if index < self.canvas.len() {
-                                    self.canvas[index] = self.color_state as u32;
+                        if let Some(surface) = &mut self.surface {
+                            let window_size = window.inner_size();
+                            let buffer_result = surface.buffer_mut();
+
+                            match buffer_result {
+                                Ok(mut softbuffer) => {
+                                    let width = window_size.width as usize;
+                                    let x = position.x as usize;
+                                    let y = position.y as usize;
+
+                                    let index = y * width + x;
+
+                                    // Write pixel directly into softbuffer instead of using self.canvas
+                                    softbuffer[index] = self.color_state as u32;
+
+                                    // Now only present this one pixel
+                                    softbuffer
+                                        .present_with_damage(&[Rect {
+                                            x: x as u32,
+                                            y: y as u32,
+                                            width: NonZeroU32::new(1).unwrap(),
+                                            height: NonZeroU32::new(1).unwrap(),
+                                        }])
+                                        .unwrap();
                                 }
-                            }
-
-                            // Update surface
-                            if let Some(surface) = &mut self.surface {
-                                let buffer_result = surface.buffer_mut();
-                                match buffer_result {
-                                    Ok(mut softbuffer) => {
-                                        for (lx, ly) in &line {
-                                            let index = ly * width + lx;
-                                            if index < softbuffer.len() {
-                                                softbuffer[index] = self.color_state as u32;
-                                            }
-                                        }
-
-                                        // Bounding box for present_with_damage
-                                        let min_x =
-                                            line.iter().map(|(x, _)| *x).min().unwrap_or(x) as u32;
-                                        let max_x =
-                                            line.iter().map(|(x, _)| *x).max().unwrap_or(x) as u32;
-                                        let min_y =
-                                            line.iter().map(|(_, y)| *y).min().unwrap_or(y) as u32;
-                                        let max_y =
-                                            line.iter().map(|(_, y)| *y).max().unwrap_or(y) as u32;
-
-                                        let rect = Rect {
-                                            x: min_x,
-                                            y: min_y,
-                                            width: NonZeroU32::new((max_x - min_x + 1).max(1))
-                                                .unwrap(),
-                                            height: NonZeroU32::new((max_y - min_y + 1).max(1))
-                                                .unwrap(),
-                                        };
-
-                                        softbuffer.present_with_damage(&[rect]).unwrap();
-                                    }
-                                    Err(err) => println!("Failed to create soft buffer: {}", err),
-                                }
+                                Err(err) => println!("Failed to create soft buffer: {}", err),
                             }
                         }
 
-                        self.last_position = Some(position);
+                        // window.request_redraw();
                     }
                 }
-
-                // if let Some(window) = &self.window {
-                //     if self.draw_state == DrawState::Drawing {
-                //         let index = (position.y as usize * window.inner_size().width as usize)
-                //             + position.x as usize;
-                //
-                //         self.canvas[index] = self.color_state as u32;
-                //
-                //         println!(
-                //             "[CursorMoved]: Position is {}x{} (i - {})",
-                //             position.x, position.y, index
-                //         );
-                //
-                //         if let Some(surface) = &mut self.surface {
-                //             let window_size = window.inner_size();
-                //             let buffer_result = surface.buffer_mut();
-                //
-                //             match buffer_result {
-                //                 Ok(mut softbuffer) => {
-                //                     // let width = window_size.width as usize;
-                //                     // let x = position.x as usize;
-                //                     // let y = position.y as usize;
-                //                     //
-                //                     // let index = y * width + x;
-                //                     //
-                //                     // // Write pixel directly into softbuffer instead of using self.canvas
-                //                     // softbuffer[index] = self.color_state as u32;
-                //                     //
-                //                     // // Now only present this one pixel
-                //                     // softbuffer
-                //                     //     .present_with_damage(&[Rect {
-                //                     //         x: x as u32,
-                //                     //         y: y as u32,
-                //                     //         width: NonZeroU32::new(1).unwrap(),
-                //                     //         height: NonZeroU32::new(1).unwrap(),
-                //                     //     }])
-                //                     //     .unwrap();
-                //                 }
-                //                 Err(err) => println!("Failed to create soft buffer: {}", err),
-                //             }
-                //         }
-                //
-                //         // window.request_redraw();
-                //     }
-                // }
             }
             WindowEvent::MouseInput {
                 device_id,
@@ -294,7 +234,8 @@ impl ApplicationHandler for DrawOnScreen {
             WindowEvent::Resized(size) => {
                 println!("[Resized]: Size is {}x{}", size.width, size.height);
                 if size.width > 0 && size.height > 0 {
-                    self.canvas = vec![0x00000000; (size.width * size.height) as usize];
+                    self.canvas =
+                        vec![ColorState::Black as u32; (size.width * size.height) as usize];
 
                     if let Some(surface) = &mut self.surface {
                         surface
@@ -326,50 +267,4 @@ fn main() {
         }
         Err(err) => println!("Failed to create event loop: {}", err),
     };
-}
-
-impl DrawOnScreen {
-    fn draw_line(
-        &mut self,
-        start: (usize, usize),
-        end: (usize, usize),
-        width: usize,
-        height: usize,
-    ) -> Vec<(usize, usize)> {
-        let mut points = vec![];
-
-        let (x0, y0) = start;
-        let (x1, y1) = end;
-
-        let dx = (x1 as isize - x0 as isize).abs();
-        let dy = -(y1 as isize - y0 as isize).abs();
-        let sx = if x0 < x1 { 1 } else { -1 };
-        let sy = if y0 < y1 { 1 } else { -1 };
-        let mut err = dx + dy;
-
-        let mut x = x0 as isize;
-        let mut y = y0 as isize;
-
-        loop {
-            if x >= 0 && y >= 0 && x < width as isize && y < height as isize {
-                points.push((x as usize, y as usize));
-            }
-
-            if x == x1 as isize && y == y1 as isize {
-                break;
-            }
-
-            let e2 = 2 * err;
-            if e2 >= dy {
-                err += dy;
-                x += sx;
-            }
-            if e2 <= dx {
-                err += dx;
-                y += sy;
-            }
-        }
-
-        points
-    }
 }
